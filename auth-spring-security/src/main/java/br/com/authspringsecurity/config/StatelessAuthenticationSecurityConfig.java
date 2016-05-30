fda488b633cf7046c51225ba34085274962573cb
laia10 <gmllaia@gmail.com>
2016-05-30 17:43:42 -0300

    new

--- auth-spring-security/src/main/java/br/com/authspringsecurity/config/StatelessAuthenticationSecurityConfig.java
+++ auth-spring-security/src/main/java/br/com/authspringsecurity/config/StatelessAuthenticationSecurityConfig.java
@@ -1,12 +1,17 @@
 package br.com.authspringsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.com.authspringsecurity.filter.StatelessAuthenticationFilter;
@@ -46,14 +51,10 @@ public class StatelessAuthenticationSecurityConfig extends WebSecurityConfigurer
 		.antMatchers(HttpMethod.POST, "/api/login").permitAll()
 		
 		//allow anonymous GETs to API
-		.antMatchers(HttpMethod.GET, "/api/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/**").hasRole("ADMIN")
 		
-		//defined Admin only API area
-		.antMatchers("/admin/**").hasRole("ADMIN")
+		.and()
 		
-		//all other request need to be authenticated
-		.anyRequest().hasRole("USER").and()				
-
 		// custom JSON based authentication by POST of {"username":"<name>","password":"<password>"} which sets the token header upon authentication
 		.addFilterBefore(new StatelessLoginFilter("/api/login", tokenAuthenticationService, userDetailService, authenticationManager()), UsernamePasswordAuthenticationFilter.class)
 
@@ -61,4 +62,19 @@ public class StatelessAuthenticationSecurityConfig extends WebSecurityConfigurer
 		.addFilterBefore(new StatelessAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
 	}
 	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+	@Override
+	protected UserDetailsService userDetailsService() {
+		return userDetailService;
+	}
 }
